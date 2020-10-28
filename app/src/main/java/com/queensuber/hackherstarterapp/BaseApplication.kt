76428693ea404a8e2573936290b55986316d05d8
Michael Bieniek <mbieniek@uber.com>
2020-10-27 21:43:21 -0400

    Import network dependencies (retrofit, hilt, okhttp)

---
+++ app/src/main/java/com/queensuber/hackherstarterapp/BaseApplication.kt
@@ -0,0 +1,7 @@
+package com.queensuber.hackherstarterapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BaseApplication: Application()
---
+++ app/src/main/java/com/queensuber/hackherstarterapp/api/NewsService.kt
@@ -0,0 +1,28 @@
+package com.queensuber.hackherstarterapp.api
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+interface NewsService {
+    companion object {
+        private const val BASE_URL = "http://newsapi.org/"
+
+        fun create(): NewsService {
+            val logger = HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BASIC }
+
+            val client = OkHttpClient.Builder()
+                .addInterceptor(logger)
+                .build()
+
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(client)
+                .addConverterFactory(MoshiConverterFactory.create())
+                .build()
+                .create(NewsService::class.java)
+        }
+    }
+}
+
