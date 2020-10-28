e4e02f7d8c0bfc9ba40ac0e9c186aee5ba6f6171
Michael Bieniek <mbieniek@uber.com>
2020-10-28 02:05:32 -0400

    Renamed dashboard fragment to social fragment

--- app/src/main/java/com/queensuber/hackherstarterapp/ui/social/SocialFragment.kt
+++ app/src/main/java/com/queensuber/hackherstarterapp/ui/social/SocialFragment.kt
@@ -1,4 +1,4 @@
-package com.queensuber.hackherstarterapp.ui.dashboard
+package com.queensuber.hackherstarterapp.ui.social
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -10,20 +10,20 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.queensuber.hackherstarterapp.R
 
-class DashboardFragment : Fragment() {
+class SocialFragment : Fragment() {
 
-    private lateinit var dashboardViewModel: DashboardViewModel
+    private lateinit var socialViewModel: SocialViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
-        val textView: TextView = root.findViewById(R.id.text_dashboard)
-        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
+        socialViewModel =
+            ViewModelProvider(this).get(SocialViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_social, container, false)
+        val textView: TextView = root.findViewById(R.id.text_social)
+        socialViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return root
--- app/src/main/java/com/queensuber/hackherstarterapp/ui/social/SocialViewModel.kt
+++ app/src/main/java/com/queensuber/hackherstarterapp/ui/social/SocialViewModel.kt
@@ -1,13 +1,13 @@
-package com.queensuber.hackherstarterapp.ui.dashboard
+package com.queensuber.hackherstarterapp.ui.social
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class SocialViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "This is the social fragment"
     }
     val text: LiveData<String> = _text
 }
